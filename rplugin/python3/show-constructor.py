a9c3884fafe1daefe7c9fc7bdec864e141616f3e
dominic-wedesign <dominic@we-design.eu>
2017-12-23 00:21:35 +0100

    redesign

--- rplugin/python3/show-constructor.py
+++ rplugin/python3/show-constructor.py
@@ -1,3 +1,4 @@
+import abc
 import neovim
 import glob
 import os
@@ -11,65 +12,80 @@ class Main(object):
     @neovim.function('ShowConstructor')
     def showConstructor(self, args):
         current_word = word = self.nvim.eval('expand("<cword>")')
+        buffer_ = self.nvim.current.buffer
         directory = self.nvim.eval('getcwd()')
         extension = self.nvim.eval('expand("%:e")')
 
-        os.chdir(directory)
-        search_string = "{}/**/{}.{}".format(directory, current_word, extension)
-        result = glob.glob(search_string)
-        found = 'no'
-        f = None
-        if(len(result) == 1):
-            f = result[0]
-            found = 'yes'
-        if (not f):
-            return
-
-        c = Constructor(extension, f, current_word)
-        constructors = c.get_constructors()
+        class_name = extension.title() + 'Constructor'
+        constructor_handler = globals()[class_name](extension, directory,\
+                                                    current_word, self.nvim)
+        constructors = constructor_handler.get_constructors()
 
         message = " ## ".join(constructors)
         command = 'echom "{}"'.format(message)
         self.nvim.command(command)
-
-        # command = 'echo "{}{}"'.format(current_word, directory)
-        # self.nvim.command(command)
-
-
-        # command = 'echo "{}"'.format(found)
-        # self.nvim.command(command)
 
     def _char_at(self, row, col):
         return self.nvim.current.buffer[row][col]
 
 
 class Constructor:
-    def __init__(self, language, file_, name):
+    def __init__(self, language, directory, name, nvim):
         self._language = language
-        self._f = file_
-        self.name = name
+        self._directory = directory
+        self._name = name
+        self._nvim = nvim
 
     """
     Return list of constructors
     """
+    @abc.abstractmethod
     def get_constructors(self):
-        return self.__getattribute__('_' + self._language)()
+        return
 
+class JavaConstructor(Constructor):
     """
     Return java constructors
     """
-    def _java(self):
-        pattern = "public " # + self.name + " ?\(.*\) ?\{"
-        pattern = r"public "# + re.escape(self.name)# + r"\s?\(.*\)\s?u\{"
-        pattern = r".*public " + re.escape(self.name)
+    def __init__(self, language, directory, name, nvim):
+        super().__init__(language, directory, name, nvim)
+
+    """
+    Return the file containing the constructor
+    """
+    def _get_constructor_file(self):
+        os.chdir(self._directory)
+        search_string = "{}/**/{}.{}".format(self._directory, self._name,\
+                                             self._language)
+        result = glob.glob(search_string, recursive=True)
+        file_ = None
+        if(len(result) == 1):
+            file_ = result[0]
+        if(len(result) > 1):
+            choices = "\n&".join(["{}. {}".format(i+1, r) for i, r in enumerate(result)])
+            self._nvim.command('call inputsave()')
+            command = "let user_input = confirm('Choose a source', '&{}', 1)".format(choices)
+            self._nvim.command(command)
+            self._nvim.command('call inputrestore()')
+            answer = self._nvim.eval('user_input')
+            file_ = result[answer-1]
+            self._nvim.command("echom '{}'".format(answer))
+
+        return file_
+
+    def get_constructors(self):
+        pattern = r".*public " + re.escape(self._name)
         prog = re.compile(pattern)
         constructors = []
 
-        with open(self._f) as f:
+        file_ = self._get_constructor_file();
+        if (not file_):
+            return
+
+        with open(file_) as f:
             for l in f.readlines():
                 if (prog.match(l)):
-                    #end = l.index(')')
-                    # constructors.append(l.strip())
-                    constructors.append(l.strip()[:l.find("\{")])
+                    # append stripped
+                    constructors.append(l.strip()[6:l.find("\{")])
         return constructors
 
